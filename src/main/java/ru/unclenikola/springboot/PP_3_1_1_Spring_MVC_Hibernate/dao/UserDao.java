89b60584f614d96ff2a81f2bb435bc3ff51e99c1
unclenikola <9107066065@mail.ru>
2025-03-17 14:11:35 +0300

    ok

---
+++ src/main/java/ru/unclenikola/springboot/PP_3_1_1_Spring_MVC_Hibernate/dao/UserDao.java
@@ -0,0 +1,13 @@
+package web.dao;
+
+import web.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+    List<User> getAllUsers();
+    User getUserById(Long id);
+    void addUser(User user);
+    void updateUser(User user);
+    void deleteUser(Long id);
+}
---
+++ src/main/java/ru/unclenikola/springboot/PP_3_1_1_Spring_MVC_Hibernate/dao/UserDaoImpl.java
@@ -0,0 +1,46 @@
+package web.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import org.springframework.stereotype.Repository;
+import web.model.User;
+
+
+import java.util.List;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<User> getAllUsers() {
+        TypedQuery<User> query = entityManager.createQuery("SELECT u FROM User u", User.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return entityManager.find(User.class, id);
+    }
+
+    @Override
+    public void addUser(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    public void updateUser(User user) {
+        entityManager.merge(user);
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        User user = entityManager.find(User.class, id);
+        if (user != null) {
+            entityManager.remove(user);
+        }
+    }
+}
